ead47fe5cfcf20d47b0545a3a2778e1a50f882eb
Your Name <you@example.com>
2024-04-07 17:16:10 +0500

    init

--- src/ts/service/Cart.ts
+++ src/ts/service/Cart.ts
@@ -9,9 +9,17 @@ export default class Cart {
     get items(): Buyable[] {
         return [...this._items]; 
     }
+
+    getSum(items: Buyable[]): number {
+        return items.reduce((acc: number, item: Buyable) => acc + item.price, 0)
+    }
+    
+    getSumSale(items: Buyable[], sale: number): number {
+        return this.getSum(items) * (100 - sale) * 0.01
+    }
+    
+    deletedId(id: number): void {
+        this._items = this._items.filter((item: Buyable) => item.id !== id);
+    }
 }
 
-
-function price() {
-    return  
-}
